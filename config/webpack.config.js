6839030dc8a67be52568f8b2b7af67b1a81a98bd
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-12-11 21:08:34 +0545

    Add scss

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
       use: {
         loader: 'babel-loader'
       }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   plugins: [
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './myStyles.scss';
+
 const App = () => {
-  return (
-    <div>
-      <p>United Fan</p>
-      <h1>AJRedDevil</h1>
-    </div>
+  return ( <
+    div className = "app" >
+    <
+    p > United Fan < /p> <
+    h1 > AJRedDevil < /h1> <
+    /div>
   );
 }
 
-ReactDOM.render(
-  <App/>, document.getElementById('app'));
+ReactDOM.render( <
+  App / > , document.getElementById('app'));
