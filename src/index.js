c948ac2f439f8450a73e2c04ef9bc926cceb2ffc
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-12-14 21:39:20 +0545

    Applying ESLint

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ class App extends React.Component {
         CaptainKirkBio: character,
       });
     } catch (error) {
+      // eslint-disable-next-line
       console.log('error', error);
     }
   };
@@ -46,9 +47,9 @@ class App extends React.Component {
       <div className="app">
         <img alt="header" src="/dist/images/header.jpg" className="app-header" />
         <p>
-          We are a most promising species, Mr.Spock, as predators go.Did you know that ? I
-          frequently have my doubts.I dont.Not any more.And maybe in a thousand years or so, we will
-          be able to prove it.{' '}
+          We are a most promising species, Mr.Spock, as predators go.Did you know that ? I I
+          frequently have my doubts.I dont.Not any more.And maybe in a thousand years or so will be
+          able to prove it.{' '}
         </p>{' '}
         <p> -Captain Kirk </p>{' '}
         <section>
